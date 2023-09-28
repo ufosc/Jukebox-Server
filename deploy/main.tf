85ae674a237e84954974d5bc34b1f1bb4278b70d
Isaac Hunter <ikehunter5@gmail.com>
2023-09-28 14:48:47 -0400

    add files and todos for deployment stage 1

--- deploy/main.tf
+++ deploy/main.tf
@@ -11,3 +11,15 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManagedBy   = "Terraform"
+  }
+}
+
+data "aws_region" "current" {}
---
+++ deploy/network.tf
@@ -0,0 +1,22 @@
+/**
+NETWORK CONFIGURATION.
+
+The network configuration will include the following aws resources:
+- VPC (Virtual Private Cloud)
+- Subnets (public & private)
+*/
+
+#################################################
+# VPC - Contains Isolated Network Configuration #
+#################################################
+# TODO: Create vpc
+
+#####################################################
+# Public Subnets - Inbound/Outbound Internet Access #
+#####################################################
+# TODO: Create public subnets
+
+####################################################
+# Private Subnets - Outbound internet access ounly #
+####################################################
+# TODO: Create private subnets

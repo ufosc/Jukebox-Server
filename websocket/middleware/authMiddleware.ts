ab9ac975ab46cc754c35c4072565bbc77ecdf885
Isaac Hunter <ikehunter5@gmail.com>
2024-09-05 12:07:41 -0400

    connect ws to frontend

--- websocket/middleware/authMiddleware.ts
+++ websocket/middleware/authMiddleware.ts
@@ -6,6 +6,7 @@ export const authMiddleware = (socket: CustomSocket, next: SocketNext) => {
   if (BYPASS_AUTH) return next()
 
   const userId = socket.handshake.auth.userId
+
   if (!userId) {
     logger.warn('Client does not have valid user id.')
     return next(new Error('Must include valid user id.'))

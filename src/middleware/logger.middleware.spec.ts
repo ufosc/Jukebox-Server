130a7c9089c78e4c1f05eb962f55c55f43403a2c
Isaac Hunter <ikehunter5@gmail.com>
2025-04-10 18:50:45 -0400

    removed logger test

--- src/middleware/logger.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RequestLoggerMiddleware } from './logger.middleware'
-
-describe('RequestLoggerMiddleware', () => {
-  it('should be defined', () => {
-    expect(new RequestLoggerMiddleware()).toBeDefined()
-  })
-})
